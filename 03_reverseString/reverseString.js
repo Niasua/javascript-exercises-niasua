5dcefc0e8042526021790e58f958b6e2fab8bec4
niasua <niasua@Nico-PC.>
2025-03-01 02:11:04 +0000

    reverseString Completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reversed = "";
+    for (let i = 0; i < word.length; i++) {
+        reversed = word[i] + reversed;
+    }    
+    return reversed;
 };
 
 // Do not edit below this line
