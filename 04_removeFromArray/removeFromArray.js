bd008ab9be98847cbac152ee84f4f7e82b579635
niasua <niasua@Nico-PC.>
2025-03-01 14:01:37 +0000

    removeFromArray Completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+    const newArray = array.filter(item => !toRemove.includes(item));    
+    return newArray;
 };
+
+removeFromArray([1,2,3], 2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
