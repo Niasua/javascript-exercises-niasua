3d50676ca334fb9b72087c87bc173ce21ab4bb4d
niasua <niasua@Nico-PC.>
2025-03-01 14:56:18 +0000

    sumAll Completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (start < 0 | end < 0 | !Number.isInteger(start) | !Number.isInteger(end)) {
+        return "ERROR";
+    }
+    let sum = 0;
+    let swap = 0;
+    if(start >= end) {
+        swap = start;
+        start = end;
+        end = swap;
+    }
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
