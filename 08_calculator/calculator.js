462850e7e6c90a2ec85e0eed68a4e6c7a14692af
niasua <niasua@Nico-PC.>
2025-03-02 23:56:57 +0000

    calculator Completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,32 @@
-const add = function() {
-	
-};
+const add = (a, b) => a + b;
 
-const subtract = function() {
-	
-};
+const subtract = (a, b) => a - b;
 
-const sum = function() {
-	
-};
+const sum = (sum) => {
+  result = 0;
+  sum.forEach(element => {
+    result += element;
+  });
+  return result;
+}
 
-const multiply = function() {
+const multiply = (n) => {
+  result = 1;
+  n.forEach(element => {
+    result *= element;
+  });
+  return result;
+}
 
-};
+const power = (a, b) => a ** b;
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = (n) => {
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
 
 // Do not edit below this line
 module.exports = {
