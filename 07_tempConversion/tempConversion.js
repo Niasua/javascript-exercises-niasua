8f190a93ccb13aacc8fd7edfd8cbba5e7207142a
niasua <niasua@Nico-PC.>
2025-03-02 23:24:43 +0000

    tempConversion Completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,5 +1,5 @@
-const convertToCelsius = farenheit => Math.round((farenheit - 32) * 5/9) + (((farenheit - 32) * 5/9)-(farenheit - 32) * 5/9).toFixed(1);
-const convertToFahrenheit = celcius => Math.round((celcius * 9/5) + 32)+(((celcius * 9/5) + 32)-((celcius * 9/5) + 32).toFixed(1));
+const convertToCelsius = farenheit => parseFloat(((farenheit - 32) * 5/9).toFixed(1));
+const convertToFahrenheit = celcius => parseFloat(((celcius * 9/5) + 32).toFixed(1));
 
 // Do not edit below this line
 module.exports = {
