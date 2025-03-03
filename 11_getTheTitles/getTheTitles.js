69ca3d144e03747d88b3982658f54d6958fbc843
niasua <niasua@Nico-PC.>
2025-03-03 01:28:19 +0000

    getTheTitles Completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,7 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = books => {
+    let titles = books.map(book => book.title);
+    return titles;
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
