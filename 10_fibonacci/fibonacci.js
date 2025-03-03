dbe4251faced66d2f347dc8fd8e21ab2a80c4f19
niasua <niasua@Nico-PC.>
2025-03-03 01:11:45 +0000

    fibonacci Completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+const fibonacci = n => {
+    n = Number(n);
 
+    if (n < 0) return "OOPS";
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+
+    let a = 0, b = 1, temp;
+    
+    for (let i = 2; i <= n; i++) {
+        temp = a + b;
+        a = b;
+        b = temp;
+    }
+
+    return b;
 };
+
+module.exports = fibonacci;
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
+ 
